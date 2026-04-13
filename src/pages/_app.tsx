a48f6f7ebf42c2ea2d06fb2eb05e02227795fcad
softgenai <admin@softgen.ai>
2026-04-13 16:07:46 +0000

    feat(ui): add fade-in page transitions with framer-motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
-import { Toaster } from "@/components/ui/toaster";
-import { CasaProvider } from "@/contexts/CasaContext";
-import { AnimatedBackground } from "@/components/AnimatedBackground";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
+import { CasaProvider } from "@/contexts/CasaContext";
+import { Toaster } from "@/components/ui/toaster";
+import { AnimatePresence } from "framer-motion";
+import { PageTransition } from "@/components/PageTransition";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
-    <CasaProvider>
-      <AnimatedBackground />
-      <Component {...pageProps} />
-      <Toaster />
-    </CasaProvider>
+    <ThemeProvider>
+      <CasaProvider>
+        <AnimatePresence mode="wait" initial={false}>
+          <PageTransition key={router.asPath}>
+            <Component {...pageProps} />
+          </PageTransition>
+        </AnimatePresence>
+        <Toaster />
+      </CasaProvider>
+    </ThemeProvider>
   );
 }
